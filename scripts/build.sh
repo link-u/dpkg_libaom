0f0a3c38648632d0b43ea0626b3720b935eb4028
psi <ryo.hirafuji@link-u.co.jp>
2020-06-05 16:00:55 +0900

    change target

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,5 @@ SCRIPT_PATH=$(readlink -f $(cd $(dirname $0) && pwd))
 cd ${SCRIPT_PATH}
 
 env --chdir=../libaom fakeroot debian/rules clean
-env --chdir=../libaom fakeroot debian/rules configure
+env --chdir=../libaom fakeroot debian/rules build
 env --chdir=../libaom fakeroot debian/rules binary
